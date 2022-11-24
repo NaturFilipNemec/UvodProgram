7e15a70a94261a627549cf4b9db4c061bc4aaee7
Filip Němec <nemecfi1@natur.cuni.cz>
2022-11-24 19:11:15 +0100

    zaciname

---
+++ ukol2.py
@@ -0,0 +1,3 @@
+import csv
+with open ("vstup.csv", encoding="utf -8",newline='')as vstup,  open('vystup_7dni.csv', mode='w',encoding="utf -8",newline='') as sedmdnu,  \
+     open('vystup_rok', mode='w',encoding="utf -8",newline='') as rok :
