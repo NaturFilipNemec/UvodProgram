0f6f93b8bb153780770873d19cdcbb87b73907a3
Filip Němec <nemecfi1@natur.cuni.cz>
2022-11-24 20:42:08 +0100

    snazim se

--- ukol2.py
+++ ukol2.py
@@ -1,3 +1,15 @@
 import csv
-with open ("vstup.csv", encoding="utf -8",newline='')as vstup,  open('vystup_7dni.csv', mode='w',encoding="utf -8",newline='') as sedmdnu,  \
+tyden_list=[]
+def dokument():
+    with open ("vstup.csv", encoding="utf -8",newline='')as vstup,  open('vystup_7dni.csv', mode='w',encoding="utf -8",newline='') as sedmdnu,  \
      open('vystup_rok', mode='w',encoding="utf -8",newline='') as rok :
+            reader = csv.DictReader(vstup, delimiter=";")   
+            next(reader) #uvedom si jak vypada prvni radek
+            writer1=csv.writer(sedmdnu)
+            writer2=csv.writer(rok)
+def sedm_dnu():
+    for row in reader:
+        while tyden_list < len(tyden_list):
+            if row["datum"] -1  == row ["datum"+1]
+            tyden_list.append ( row["prutok"] )     # zjistit jak se jmenuje den radek s prutokem
+
