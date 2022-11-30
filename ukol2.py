ea4da28874ffd924dff261f5714c1d098e99a6a9
Filip Němec <nemecfi1@natur.cuni.cz>
2022-11-30 09:21:37 +0100

    minum a maximun hotovo

--- ukol2.py
+++ ukol2.py
@@ -5,8 +5,8 @@ rok_list=[]
 rok_celkem = 0
 aktualni_rok = 0
 cislo_radku = 0
-Max_prutok=0
-min_prutok= 0 
+Max_prutok=[]
+min_prutok= []
 with open ("vstup.csv", mode='r' , encoding="utf -8",newline='') as vstup , open('vystup_7dni.csv', mode='w',encoding="utf -8",newline='') as sedmdnu, \
      open('vystup_rok.csv', mode='w',encoding="utf -8",newline='') as rok :
         reader = csv.reader(vstup, delimiter=",")   
@@ -16,15 +16,15 @@ with open ("vstup.csv", mode='r' , encoding="utf -8",newline='') as vstup , open
             if len(row) !=4:
                 raise Exception("Vstupní data nemají správný počet sloupců(4)")
             cislo_radku += 1
-            if Max_prutok==0 and min_prutok==0 :
-                Max_prutok=row[2:3]
-                min_prutok=row[2:3]
-            if Max_prutok < row[3]:
-                Max_prutok = row[3]
-                print( Max_prutok)
-            if min_prutok < row[3]:
-                min_prutok = row[3]
-
+            if len(Max_prutok)==0 and len (min_prutok)==0 :
+                Max_prutok=[row[2] , row[3]]
+                min_prutok=[row[2] , row[3]]
+            if float(Max_prutok[1]) < float(row[3]):
+                Max_prutok = [row[2], row[3]]
+            if float(min_prutok[1]) > float(row[3]):
+                min_prutok = [row[2] , row[3]]
+print( Max_prutok)
+print (min_prutok)
 
             
 
